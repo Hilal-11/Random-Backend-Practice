44499663add130cedc2705e799c81c05e6adda2d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-03 23:50:40 +0530

    routes

--- practice Blogging/routes/appRoutes.js
+++ practice Blogging/routes/appRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get()
-
+router.get('/getPosts' , getPosts)
+router.post('/createPost' , createPost)
+router.post('/like:id', like)
+router.post('comment:id', comment)
+router.post('dislike:id', dislike)
 
 module.exports = router
