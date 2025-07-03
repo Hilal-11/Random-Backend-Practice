0377f496f4c4b52b07a14ee0e7d6c2ce95d6a6da
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-03 23:54:11 +0530

    define different routes

--- practice Blogging/routes/appRoutes.js
+++ practice Blogging/routes/appRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const getPosts = require('../controllers/getPosts')
+const createPost = require('../controllers/createPost')
+const like = require('../controllers/like')
+const comment = require('../controllers/comment')
+const dislike = require('../controllers/dislike')
+
 router.get('/getPosts' , getPosts)
 router.post('/createPost' , createPost)
 router.post('/like:id', like)
