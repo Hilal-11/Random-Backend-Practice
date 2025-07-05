21722cdb44b2cada832b141932af471370116067
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-05 22:31:33 +0530

    initialize the MERN app

--- Backend App Blogs/main.js
+++ Backend App Blogs/main.js
@@ -1,4 +1,12 @@
 const express = require('express');
 require('dotenv').config()
+const app_routes = require("")
 const PORT = process.env.PORT
+
 const app = express()
+app.use(express.json())
+app.use("/api/v1" , app_routes)
+
+app.listen(PORT , () => {
+    console.log(`App is running on PORT: ${PORT}`)
+})
