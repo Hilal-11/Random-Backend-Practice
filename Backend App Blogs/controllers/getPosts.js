ebe3b02724bcebd5866742e6a49594735e696bbb
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-06 13:59:11 +0530

    get controoller

---
+++ Backend App Blogs/controllers/getPosts.js
@@ -0,0 +1,19 @@
+const Post = require('../Models/PostModel')
+const getPosts = async (req , res) => {
+    try{
+        const response = await Post.find({})
+        res.status(200).json({
+            success: true,
+            response: response,
+            message: "Successfully get all posts"
+        })
+    }catch(error) {
+        console.log(error.message)
+        res.status(400).json({
+            success: false,
+            error: error.messsage,
+            message: "Failed to get the posts"
+        })
+    }
+}
+module.exports = getPosts
