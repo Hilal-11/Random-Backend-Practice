e8d2145214958a9d49132fffdf4778c1d652770c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-13 11:23:28 +0530

    middlewares

---
+++ Auth/middlewares/auth.js
@@ -0,0 +1,27 @@
+
+// Authentication route
+const auth = async () => {
+    try{
+
+    }catch(error){
+
+    }
+}
+
+// Admin route
+const isAdmin = async () => {
+    try{
+
+    }catch(error) {
+
+    }
+}
+
+// Student route
+const isStudent = async () => {
+    try{
+
+    }catch(error) {
+
+    }
+}
--- Auth/routes/app_routes.js
+++ Auth/routes/app_routes.js
@@ -3,8 +3,23 @@ const router = express.Router();
 
 const signup = require('../controllers/signup')
 const login = require('../controllers/login')
+const { auth , isAdmin , isStudent } = require('../middlewares/auth')
 
 router.post('/signup' , signup);
 router.post('/login' , login);
+
+router.get('/student' , auth , isStudent , (req , res) => {
+    res.json({
+        success: true,
+        message: "Wel come to the protected route for Student"
+    })
+})
+
+router.get('/admin' , isAdmin , (req , res) => {
+    res.json({
+        success: true,
+        message: "Wel come to the protected route for Admin"
+    })
+})
 
 module.exports = router;
