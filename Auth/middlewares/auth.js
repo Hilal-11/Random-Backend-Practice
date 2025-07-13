41cb47b0685b8bc989811fd6bf82e924ce765b00
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-13 11:48:02 +0530

    authentication and autherization middlewares

--- Auth/middlewares/auth.js
+++ Auth/middlewares/auth.js
@@ -1,27 +1,77 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
 // Authentication route
-const auth = async () => {
+exports.auth = async (req , res , next) => {
     try{
-
+        // other ways to get the token
+        const { token } = req.body.token || req.cookies.token
+        if(!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Token missing"
+            })
+        }
+        // decode token and find the role from token using JWT // varify the token
+        try{
+            const decode = jwt.verify(token , process.env.SECRET_KEY);
+            console.log(decode)
+            req.user = decode
+        }catch(error){
+            return res.status(401).json({
+                success: false,
+                message: "token is invalid"
+            })
+        }
+        next()
     }catch(error){
-
+        console.log(error.message)
+        return res.status(401).json({
+            success: false,
+            error: error.message,
+            message: "failed to authenticate...something went wrong"
+        })
     }
 }
 
 // Admin route
-const isAdmin = async () => {
+exports.isAdmin = async (req , res , next) => {
     try{
+        if(req.user.role !== "Admin") {
+            return res.status(401).json({
+                success: true,
+                message: "This is protected route for Admin"
+            });
+        }
 
+        next()
     }catch(error) {
-
+        console.log(error)
+        return res.status(401).json({
+            success: false,
+            error: error.message,
+            message: "user role can't be varified or matching"
+        })
     }
 }
 
 // Student route
-const isStudent = async () => {
+exports.isStudent = async (req , res , next) => {
     try{
+        if(req.user.role !== "Student") {
+            return res.status(401).json({
+                success: true,
+                message: "This is protected route for students"
+            });
+        }
 
+        next()
     }catch(error) {
-
+        console.log(error)
+        return res.status(401).json({
+            success: false,
+            error: error.message,
+            message: "user role can't be varified or matching"
+        })
     }
 }
