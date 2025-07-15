028bfb150c9477a15e5fe4a6be4d5a9003ffdab0
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-07-15 11:31:37 +0530

    done main.js

---
+++ Auth Practice Youtube/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const appRoutes = require('./routes/app_routes');   
+const app = express(); 
+require('dotenv').config(); 
+const PORT =process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use('/api/v1' , appRoutes); // Use app routes
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Auth Practice API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+// Connect to the database
+
+connectDB();
+
+// alternative 
+
+// connectDB().then( () => {
+//     console.log("Database connection established");
+// } ).catch((error) => {
+//     console.error("Database connection failed:", error);
+//    process.exit(1);
+// });
